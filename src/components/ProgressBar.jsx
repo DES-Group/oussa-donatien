ea9f57215a8100b8c8c35ab3b48f00609bc1375f
DES-Group <digitaliserentreprise@gmail.com>
2024-08-26 00:12:08 +0100

    Modification de Tech

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import { motion } from 'framer-motion';
+import PropTypes from 'prop-types'
+
 
 const ProgressBar = ({ name, percentage, color, icon }) => {
   const radius = 100; // Rayon du cercle
@@ -8,7 +9,7 @@ const ProgressBar = ({ name, percentage, color, icon }) => {
   const offset = circumference - (percentage / 100) * circumference;
 
   return (
-    <div className="relative w-80 h-80 m-6 flex items-center justify-center">
+    <div className="relative w-40 h-40 m-6 flex items-center justify-center">
       {/* SVG avec cercle de fond */}
       <svg className="w-full h-full transform -rotate-90" viewBox="0 0 240 240">
         <circle
@@ -40,16 +41,24 @@ const ProgressBar = ({ name, percentage, color, icon }) => {
         <motion.img
           src={icon}
           alt={name}
-          className="w-32 h-32 mb-2" // Taille de l'icône ajustée
+          className="w-10 h-10 mb-2" // Taille de l'icône ajustée
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1.5, ease: "easeOut" }}
         />
-        <span className="text-4xl font-semibold text-white">{percentage}%</span>
-        <span className="text-lg text-gray-400">{name}</span>
+        <span className="text-xl font-semibold text-white">{percentage}%</span>
+        <span className="text-xl text-gray-400">{name}</span>
       </div>
     </div>
   );
 };
+
+
+ProgressBar.propTypes = {
+  name: PropTypes.string, 
+  percentage: PropTypes.string, 
+  color: PropTypes.string, 
+  icon: PropTypes.string
+}
 
 export default ProgressBar;
